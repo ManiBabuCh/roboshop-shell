dccd3f2f4d900d18be132deb0661f3481366ebb6
Mani <manibabusrkr@gmail.com>
2023-08-26 19:29:34 +0530

    mc5

--- mongodb.sh
+++ mongodb.sh
@@ -3,7 +3,7 @@ cp mongodb.repo /etc/yum.repos.d/mongo.repo
 echo -e "\e[32m installing mongodb\e[0m"
 yum install mongodb-org -y &>>/tmp/roboshop.log
 echo -e "\e[32m changing conf ip\e[0m"
-sed -i 's/127.0.0.1/0.0.0.0' /etc/mongod.conf &>>/tmp/roboshop.log
+sed -i 's/127.0.0.1/0.0.0.0' /etc/mongod.conf
 echo -e "\e[32m mongodb restart\e[0m"
 systemctl enable mongod &>>/tmp/roboshop.log
 systemctl restart mongod &>>/tmp/roboshop.log
