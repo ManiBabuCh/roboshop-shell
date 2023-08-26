718511ebf3602b11888ac27f7f11604df4438be4
Mani <manibabusrkr@gmail.com>
2023-08-26 13:37:10 +0530

    fc3

--- frontend.sh
+++ frontend.sh
@@ -5,14 +5,14 @@ yum install nginx -y
 
 #remove old content
 echo -e "\e[32m remove old content\e[0m"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* >/tmp/roboshop.log
 #downloading content
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip >/tmp/roboshop.log
 
 #unzip
 echo -e "\e[32m unzip\e[0m"
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+cd /usr/share/nginx/html >/tmp/roboshop.log
+unzip /tmp/frontend.zip >/tmp/roboshop.log
 
 
 #copy conf file
@@ -20,5 +20,5 @@ echo -e "\e[32m copy conf file\e[0m"
 
 #systemservices
 echo -e "\e[32m systemservices\e[0m"
-systemctl enable nginx
-systemctl restart nginx
+systemctl enable nginx >/tmp/roboshop.log
+systemctl restart nginx >/tmp/roboshop.log
